1cea48c5a480dad30a3ac7fd6c7cd6ab034c358a
Surgie Finesse <finesserus@gmail.com>
2020-12-09 21:00:24 +1000

    fix: enable FPJS agent monitoring

--- src/context/FpjsContext.tsx
+++ src/context/FpjsContext.tsx
@@ -1,6 +1,12 @@
 import React, { useEffect, useState, useContext } from 'react'
 import { FullIpExtendedGetResult, GetOptions, Region } from '@fingerprintjs/fingerprintjs-pro'
-import { FPJS_TOKEN, FPJS_ENDPOINT, FPJS_REGION } from '../constants/env'
+import {
+  FPJS_TOKEN,
+  FPJS_ENDPOINT,
+  FPJS_REGION,
+  FPJS_MONITORING_CLIENT_ID,
+  FPJS_MONITORING_TOKEN,
+} from '../constants/env'
 
 const FpjsContext = React.createContext<{ visitorData?: FullIpExtendedGetResult }>({})
 const config: GetOptions<true, 'full'> = {
@@ -15,19 +21,30 @@ export function FpjsProvider({ children }: { children: React.ReactNode }) {
   const clientToken = FPJS_TOKEN ?? 'test_client_token'
   const endpoint = FPJS_ENDPOINT ?? ''
   const region = FPJS_REGION as Region
+  const monitoringClientId = FPJS_MONITORING_CLIENT_ID ?? ''
+  const monitoringToken = FPJS_MONITORING_TOKEN ?? ''
   const [visitorData, setVisitorData] = useState<FullIpExtendedGetResult>()
 
   useEffect(() => {
     async function getVisitorData() {
       const FP = await import('@fingerprintjs/fingerprintjs-pro')
-      const fp = await FP.load({ token: clientToken, endpoint, region })
+      const fp = await FP.load({
+        token: clientToken,
+        endpoint,
+        region,
+
+        // It may break after a @fingerprintjs/fingerprintjs-pro update. Please check when updating.
+        debug: monitoringToken
+          ? FP.makeRemoteDebugger({ clientId: monitoringClientId, token: monitoringToken })
+          : undefined,
+      })
       const result = await fp.get(config)
 
       setVisitorData(result)
     }
 
     getVisitorData()
-  }, [clientToken, endpoint, region])
+  }, [clientToken, endpoint, region, monitoringClientId, monitoringToken])
 
   return <FpjsContext.Provider value={{ visitorData }}>{children}</FpjsContext.Provider>
 }
