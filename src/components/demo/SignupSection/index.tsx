958076948df57a1bbd168cfc9f3ca43c0911d8ef
tan-90 <tan-90@outlook.com>
2021-01-08 18:04:58 -0300

    Reverted changes to list items on demo page.

--- src/components/demo/SignupSection/index.tsx
+++ src/components/demo/SignupSection/index.tsx
@@ -18,7 +18,24 @@ export default function SignupSection() {
           <h2 className={styles.header}>
             <strong className={styles.strong}>Sign Up</strong> Today!
           </h2>
-          <GetStartedForm />
+          <GetStartedForm
+            bullets={[
+              { text: '10 Day Trial' },
+              { text: 'API & Webhooks' },
+              {
+                text: 'GDPR and CCPA Compliant',
+                info: (
+                  <span>
+                    FingerprintJS is GDPR/CCPA compliant. Our technology is intended to be used for fraud detection only
+                    - for this use case, no user consent is required.
+                    <br />
+                    Any use outside of fraud detection would need to comply with GDPR/CCPA user consent rules. We never
+                    automatically track traffic, and never do cross-domain tracking.
+                  </span>
+                ),
+              },
+            ]}
+          />
         </div>
 
         <BackgroundImage Tag='span' className={styles.background} fluid={mainBackground}>
