0c19d5349a6f4669bd4cd677ec1463f42997bfa5
tan90 <tan-90@outlook.com>
2021-01-20 09:57:44 -0300

    fix: removed logo swipper and added more logos.

--- src/components/ClientsSection/index.tsx
+++ src/components/ClientsSection/index.tsx
@@ -1,45 +1,43 @@
 import React from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import SwiperCore, { Pagination } from 'swiper'
-
 import Section from '../common/Section'
 import Container from '../common/Container'
 import styles from './ClientsSection.module.scss'
-import classNames from 'classnames'
-
-SwiperCore.use([Pagination])
 
 export default function ClientsSection() {
-  const clients = ['ebay', 'target', 'us-bank', 'booking', 'ameritrade', 'dell', 'agoda']
+  const clients = [
+    'ebay',
+    'target',
+    'us-bank',
+    'booking',
+    'ameritrade',
+    'dell',
+    'agoda',
+    'b_and_h',
+    'coinbase',
+    'neiman-marcus',
+    'perimeterx',
+    'sift',
+    'ticket-master',
+    'yahoo',
+  ]
 
   return (
     <Section className={styles.clients}>
-      <Container>
+      <Container size='large'>
         <header className={styles.header}>
           <h2 className={styles.title}>
             <strong>FingerprintJS</strong> is trusted by public companies and innovative startups.
           </h2>
         </header>
-        <Swiper
-          className={styles.content}
-          id='swiper-clients'
-          spaceBetween={30}
-          breakpoints={{
-            320: { slidesPerView: 1 },
-            768: { slidesPerView: 3 },
-            1024: { slidesPerView: 6 },
-          }}
-          pagination={{ clickable: true, el: '.swiper-pagination' }}
-        >
+        <div className={styles.content}>
           {clients.map((client) => {
             return (
-              <SwiperSlide key={`slide_${client}`} className={styles.slide}>
+              <span key={`slide_${client}`} className={styles.slide}>
                 <img alt={`${client} logo`} className={styles.logo} src={`/img/company-logos/${client}.svg`} />
-              </SwiperSlide>
+              </span>
             )
           })}
-          <div className={classNames('swiper-pagination', styles.pagination)} />
-        </Swiper>
+        </div>
       </Container>
     </Section>
   )
