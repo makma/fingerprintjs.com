e8a320e8a5a373e81da25312502eb735a555b4a1
npg <natanael.galfrascoli@fingerprintjs.com>
2021-03-03 09:40:00 -0300

    fixed tablet view and added background to demo

--- src/components/LiveDemoSection/index.tsx
+++ src/components/LiveDemoSection/index.tsx
@@ -3,12 +3,14 @@ import FpjsWidget from '../FpjsWidget'
 import Container from '../common/Container'
 import Section from '../common/Section'
 import YoutubeEmbed from '../YoutubeEmbed/YoutubeEmbed'
+import { useMainBackgroundImage } from '../../hooks/useBackgroundImage'
 
 import styles from './LiveDemoSection.module.scss'
 
 export default function LiveDemoSection() {
+  const { mainBackground } = useMainBackgroundImage()
   return (
-    <Section className={styles.liveDemo}>
+    <Section className={styles.liveDemo} backgroundImageFluid={mainBackground}>
       <Container size='large' className={styles.container}>
         <div className={styles.content}>
           <h2 className={styles.title}>Installing FingerprintJS Pro</h2>
