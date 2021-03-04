5d03d16805837b520cf69cb1a33214491e948400
npg <natanael.galfrascoli@fingerprintjs.com>
2021-03-04 12:07:02 -0300

    changed video title

--- src/components/LiveDemoSection/index.tsx
+++ src/components/LiveDemoSection/index.tsx
@@ -11,7 +11,7 @@ export default function LiveDemoSection() {
     <Section className={styles.liveDemo}>
       <Container size='large' className={styles.container}>
         <div className={styles.content}>
-          <h2 className={styles.title}>Installing FingerprintJS Pro</h2>
+          <h2 className={styles.title}>Getting Started With FingerprintJS Pro</h2>
           <YoutubeEmbed embedId='jWX9P5_jZn8' />
         </div>
         <div className={styles.content}>
