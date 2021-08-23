04b624b74e6bc2d4e6a27e2a99c891311c05d3c7
natapg <natanael.galfrascoli@fingerprintjs.com>
2021-08-23 14:31:25 -0300

    added firts section

---
+++ src/pages/demo2/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { LayoutTemplate } from '../../components/Layout'
+import BreadcrumbsSEO from '../../components/Breadcrumbs/BreadcrumbsSEO'
+import { GeneratedPageContext } from '../../helpers/types'
+import { URL } from '../../constants/content'
+
+import HeroSection from '../../components/HeroWithCTA/HeroWithCTA'
+
+import useSiteMetadata from '../../hooks/useSiteMetadata'
+import { useLocation } from '@reach/router'
+
+interface DemoPageProps {
+  pageContext: GeneratedPageContext
+}
+export default function DemoPage({ pageContext }: DemoPageProps) {
+  const breadcrumbs = pageContext.breadcrumb.crumbs
+  const { pathname } = useLocation()
+  let siteMetadata = useSiteMetadata()
+  siteMetadata = {
+    ...siteMetadata,
+    title: 'Technical Demo - FingerprintJS Pro',
+    siteUrl: `${siteMetadata.siteUrl}${pathname}`,
+  }
+
+  return (
+    <LayoutTemplate siteMetadata={siteMetadata}>
+      {breadcrumbs && <BreadcrumbsSEO breadcrumbs={breadcrumbs} />}
+      <HeroSection title='FingerprintJS Pro' ctaText='Start Free Trial' ctaHref={URL.dashboardLoginUrl}>
+        Identify anonymous site visitors with 99.5% accuracy to prevent online fraud
+      </HeroSection>
+    </LayoutTemplate>
+  )
+}
