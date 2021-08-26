a27bd17ca8853aa289fe8958370534b42b2aaab7
natapg <natanael.galfrascoli@fingerprintjs.com>
2021-08-26 17:41:05 -0300

    added visitorSection

---
+++ src/components/demo/DemoSection/DemoSection.tsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react'
+import useRollbar from '../../../hooks/useRollbar'
+import { VisitorResponse } from '../../../types/visitorResponse'
+import { useVisitorData } from '../../../context/FpjsContext'
+import { loadFpjsHistory } from '../../../helpers/api'
+
+import VisitorSection, { VisitorSectionLoading } from './VisitorSection/VisitorSection'
+
+export default function DemoSection() {
+  const [currentVisit, setCurrentVisit] = useState<VisitorResponse>()
+  const [visits, setVisits] = useState<VisitorResponse[]>([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  const { visitorData } = useVisitorData()
+  const visitorId = visitorData?.visitorId
+  const rollbar = useRollbar()
+
+  useEffect(() => {
+    let isCancelled = false
+    setIsLoading(true)
+
+    async function fetchVisits() {
+      if (!visitorId) {
+        return
+      }
+      try {
+        const { visits } = await loadFpjsHistory(visitorId)
+
+        if (!isCancelled) {
+          setVisits(visits)
+          setCurrentVisit(visits[0])
+        }
+      } catch (e) {
+        rollbar.error('Unable to load visits', e)
+      } finally {
+        if (!isCancelled) {
+          setIsLoading(false)
+        }
+      }
+    }
+    fetchVisits()
+
+    return () => {
+      isCancelled = true
+    }
+  }, [visitorId, rollbar])
+
+  return (
+    <>
+      {isLoading ? (
+        <VisitorSectionLoading />
+      ) : (
+        visitorId && <VisitorSection visitorId={visitorId} visits={visits} currentVisit={currentVisit} />
+      )}
+    </>
+  )
+}
---
+++ src/components/demo/DemoSection/VisitorSection/VisitorSection.tsx
@@ -0,0 +1,97 @@
+import React from 'react'
+import Section from '../../../common/Section'
+import Container from '../../../common/Container'
+import Skeleton from '../../../Skeleton/Skeleton'
+import { DOC_URL } from '../../../../constants/content'
+
+import { CurrentVisitProps } from '../../../../types/currentVisitProps'
+
+import styles from './VisitorSection.module.scss'
+
+export default function VisitorSection({ currentVisit, visitorId }: CurrentVisitProps) {
+  const loadedCard = (
+    <>
+      <div className={styles.id}>
+        <label className={styles.label}>your ID</label>
+        <p className={styles.idValue}>{visitorId}</p>
+      </div>
+      <div className={styles.info}>
+        <ul className={styles.infoList}>
+          <li>
+            <label className={styles.label}>IP</label>
+            <p className={styles.infoValue}>{currentVisit?.ip}</p>
+          </li>
+          <li>
+            <label className={styles.label}>incognito</label>
+            <p className={styles.infoValue}>{currentVisit?.incognito ? 'Yes' : 'No'}</p>
+          </li>
+          <li>
+            <label className={styles.label}>browser</label>
+            <p
+              className={styles.infoValue}
+            >{`${currentVisit?.browserDetails.browserName} on ${currentVisit?.browserDetails.os}`}</p>
+          </li>
+        </ul>
+      </div>
+    </>
+  )
+
+  return <Content card={loadedCard} />
+}
+
+export function VisitorSectionLoading() {
+  const loadingCard = (
+    <>
+      <div className={styles.id}>
+        <label className={styles.label}>your ID</label>
+        <Skeleton width={475} height={54} />
+      </div>
+      <div className={styles.info}>
+        <ul className={styles.infoList}>
+          <li>
+            <label className={styles.label}>IP</label>
+            <Skeleton width={144} height={26} />
+          </li>
+          <li>
+            <label className={styles.label}>incognito</label>
+            <Skeleton width={83} height={26} />
+          </li>
+          <li>
+            <label className={styles.label}>browser</label>
+            <Skeleton width={180} height={52} />
+          </li>
+        </ul>
+      </div>
+    </>
+  )
+
+  return <Content card={loadingCard} />
+}
+
+interface ContentProps {
+  card: React.ReactNode
+}
+function Content({ card }: ContentProps) {
+  return (
+    <Section className={styles.root}>
+      <Container className={styles.container}>
+        <section className={styles.descriptionSection}>
+          <h1 className={styles.title}>This is your visitorID</h1>
+          <p className={styles.description}>
+            A unique identifier associated with your specific browser and device. Websites can start collecting
+            visitorIDs by installing our JavaScript agent.
+          </p>
+          <a href={DOC_URL.documentationUrl} className={styles.link}>
+            See documentation →
+          </a>
+        </section>
+        <section className={styles.idSection}>
+          <div className={styles.card}>{card}</div>
+          <footer className={styles.footer}>
+            Try revisiting on VPN or incognito mode. Your visitorID will remain the same
+          </footer>
+        </section>
+      </Container>
+    </Section>
+  )
+}
