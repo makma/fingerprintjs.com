6b54f1dd72c606b5b48da62e38fa2f0b6e2eae4b
Natanael Pablo Galfrascoli <47128448+natapg@users.noreply.github.com>
2021-09-07 09:52:46 -0300

    Fix: bring back lead submit event (#284)

--- src/helpers/gtm.ts
+++ src/helpers/gtm.ts
@@ -57,3 +57,11 @@ export function enableAnalytics() {
 export function trackEmbeddedFormSubmit() {
   sendEvent({ action: EventAction.IntentSuccess, category: EventCategory.Signup, label: EventLabel.FormFill })
 }
+
+export function trackLeadSubmit(success = true) {
+  sendEvent({
+    action: EventAction.LeadSubmit,
+    category: EventCategory.Lead,
+    label: success ? EventLabel.Success : EventLabel.Error,
+  })
+}
--- src/pages/contact-sales/confirm/index.tsx
+++ src/pages/contact-sales/confirm/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import useSiteMetadata from '../../../hooks/useSiteMetadata'
+import useLocalStorage from '../../../hooks/useLocalStorage'
 import { LayoutTemplate } from '../../../components/Layout'
 
 import { useLocation } from '@reach/router'
 import { GeneratedPageContext } from '../../../helpers/types'
 import BreadcrumbsSEO from '../../../components/Breadcrumbs/BreadcrumbsSEO'
+import { trackLeadSubmit } from '../../../helpers/gtm'
 
 import { PageContent } from '../index'
 
@@ -21,6 +23,18 @@ export default function ConfirmPage({ pageContext }: ContactSalesPageProps) {
     title: 'Contact Sales - FingerprintJS Pro',
     siteUrl: `${siteMetadata.siteUrl}${pathname}`,
   }
+
+  const [isLeadSubmitEnabled, setLeadSubmitEnabled] = useLocalStorage('track_lead_submit_enabled', true)
+
+  useEffect(() => {
+    if (isLeadSubmitEnabled) {
+      setLeadSubmitEnabled(false)
+      trackLeadSubmit()
+    }
+    // ESLint rule "react-hooks/exhaustive-deps" fail on empty dependency but
+    // we need to pass an empty array as a second argument so the function will run after the first render
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <LayoutTemplate siteMetadata={siteMetadata}>
