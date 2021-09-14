49ebca1d3b825780a4cc0578595b5d3c38b6cfdf
natapg <natanael.galfrascoli@fingerprintjs.com>
2021-09-14 17:09:46 -0300

    moved logos to components

--- src/components/demo/GetStartedSection/GetStartedSection.tsx
+++ src/components/demo/GetStartedSection/GetStartedSection.tsx
@@ -4,11 +4,18 @@ import Section from '../../common/Section'
 import Container from '../../common/Container'
 import { URL } from '../../../constants/content'
 
+import { ReactComponent as EbaySVG } from '../../../../static/img/company-logos/ebay.svg'
+import { ReactComponent as TargetSVG } from '../../../../static/img/company-logos/target.svg'
+import { ReactComponent as UsBankSVG } from '../../../../static/img/company-logos/us-bank.svg'
+import { ReactComponent as BookingSVG } from '../../../../static/img/company-logos/booking.svg'
+import { ReactComponent as AgodaSVG } from '../../../../static/img/company-logos/agoda.svg'
+import { ReactComponent as CoinBaseSVG } from '../../../../static/img/company-logos/coinbase.svg'
+import { ReactComponent as CheckoutSVG } from '../../../../static/img/company-logos/checkout.svg'
+import { ReactComponent as YahooSVG } from '../../../../static/img/company-logos/yahoo.svg'
+
 import Button from '../../common/Button'
 
 export default function GetStartedSection() {
-  const clients = ['checkout', 'yahoo', 'ebay', 'coinbase', 'agoda', 'us-bank', 'booking', 'target']
-
   return (
     <Section className={styles.root}>
       <Container size='small' className={styles.heroContainer}>
@@ -20,16 +27,14 @@ export default function GetStartedSection() {
       </Container>
       <Container size='large' className={styles.clientsContainer}>
         <div className={styles.content}>
-          {clients.map((client) => {
-            return (
-              <img
-                key={`image_${client}`}
-                alt={`${client} logo`}
-                className={styles.logo}
-                src={`/img/company-logos/${client}.svg`}
-              />
-            )
-          })}
+          <CheckoutSVG className={styles.logo} />
+          <YahooSVG className={styles.logo} />
+          <EbaySVG className={styles.logo} />
+          <CoinBaseSVG className={styles.logo} />
+          <AgodaSVG className={styles.logo} />
+          <UsBankSVG className={styles.logo} />
+          <BookingSVG className={styles.logo} />
+          <TargetSVG className={styles.logo} />
         </div>
       </Container>
     </Section>
