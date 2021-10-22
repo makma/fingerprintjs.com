2c85975f3e14596154df5353bbf5a2c70fa1bd87
Natanael Pablo Galfrascoli <47128448+natapg@users.noreply.github.com>
2021-10-22 09:35:53 -0300

    fix: serve google fonts from our website (#335)

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -79,10 +79,6 @@ export function LayoutTemplate({ children, siteMetadata, headerBarTitle, headerB
         <script>
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','${gtmToken}');`}
         </script>
-        <link
-          href='https://fonts.googleapis.com/css2?family=Fira+Mono:wght@400;500;700&family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
-          rel='stylesheet'
-        />
         <link rel='preconnect' href={FPJS_ENDPOINT} />
         <link rel='preconnect' href={TLS_ENDPOINT} />
         <link rel='preconnect' href={FPJS_VISITORS_ENDPOINT} />
