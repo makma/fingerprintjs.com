74c579ec85a0693b0a2ea0b018518057cf5f42d4
Natanael Pablo Galfrascoli <47128448+natapg@users.noreply.github.com>
2021-11-29 15:58:55 -0300

    botd: added hero section static part (#396)

---
+++ src/pages/botd/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { LayoutTemplate } from '../../components/Layout'
+import BreadcrumbsSEO from '../../components/Breadcrumbs/BreadcrumbsSEO'
+import { GeneratedPageContext } from '../../helpers/types'
+
+import HeroSection from '../../components/botd/HeroSection/HeroSection'
+
+import useSiteMetadata from '../../hooks/useSiteMetadata'
+import { useLocation } from '@reach/router'
+
+interface AccountSharingProps {
+  pageContext: GeneratedPageContext
+}
+export default function Botd({ pageContext }: AccountSharingProps) {
+  const breadcrumbs = pageContext.breadcrumb.crumbs
+  const { pathname } = useLocation()
+  let siteMetadata = useSiteMetadata()
+  siteMetadata = {
+    ...siteMetadata,
+    title: 'TODO', //TODO
+    description: 'TODO', //TODO
+    siteUrl: `${siteMetadata.siteUrl}${pathname}`,
+  }
+
+  return (
+    <LayoutTemplate siteMetadata={siteMetadata}>
+      {breadcrumbs && <BreadcrumbsSEO breadcrumbs={breadcrumbs} />}
+      <HeroSection />
+    </LayoutTemplate>
+  )
+}
