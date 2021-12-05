07226e295c79e546a49d6924d645732f33067690
natapg <natanael.galfrascoli@fingerprintjs.com>
2021-12-05 20:52:10 -0300

    added error message

--- src/hooks/useBotDResponse.ts
+++ src/hooks/useBotDResponse.ts
@@ -3,27 +3,34 @@ import Botd from '@fpjs-incubator/botd-agent'
 import { BOTD_TOKEN } from '../constants/env'
 
 export const useBotDResponse = () => {
+  // TODO the botd team will add the export of the types in the next release
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const [visitorData, setVisitorData] = useState<any>()
-
-  const clientToken = BOTD_TOKEN
+  const [hasError, setHasError] = useState(false)
+  const [error, setError] = useState<string>()
 
   useEffect(() => {
     async function getVisitorData() {
       try {
         const botD = await Botd.load({
-          token: clientToken,
+          token: BOTD_TOKEN,
           mode: 'allData',
         })
         const result = await botD.detect()
         setVisitorData(result)
       } catch (error) {
-        // noop
+        setHasError(true)
+
+        if (typeof error === 'string') {
+          setError(error)
+        } else if (error instanceof Error) {
+          setError(error.message)
+        }
       }
     }
 
     getVisitorData()
-  }, [clientToken])
+  }, [])
 
-  return visitorData
+  return { visitorData, hasError, error }
 }
