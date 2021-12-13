7e6e44d83dddf0364da16d37a61ae7199264326c
natapg <natanael.galfrascoli@fingerprintjs.com>
2021-12-13 15:14:07 -0300

    added background

--- src/components/botd/JoinCommunitySection/JoinCommunitySection.tsx
+++ src/components/botd/JoinCommunitySection/JoinCommunitySection.tsx
@@ -1,35 +1,21 @@
 import React from 'react'
-import Section from '../../common/Section'
 import Container from '../../common/Container'
 import Button from '../../common/Button'
-
-import { ReactComponent as ChooseAnswerSVG } from './ChooseAnswerSVG.svg'
-import { ReactComponent as ChooseAnswerMobileSVG } from './ChooseAnswerMobileSVG.svg'
 
 import styles from './JoinCommunitySection.module.scss'
 
 export default function JoinCommunitySection() {
   return (
-    <Section className={styles.root}>
-      <section className={styles.imageSectionMobile}>
-        <ChooseAnswerMobileSVG />
-      </section>
-      <Container className={styles.container}>
-        <div className={styles.section}>
-          <section className={styles.descriptionSection}>
-            <h1 className={styles.title}>Join our growing community</h1>
-            <p className={styles.description}>
-              BotD is an open source project supported by contributing developers across the globe.
-            </p>
-            <Button href='/case-studies/edtech/' variant='outline' size='big' className={styles.button}>
-              Discord
-            </Button>
-          </section>
-          <section className={styles.imageSection}>
-            <ChooseAnswerSVG />
-          </section>
-        </div>
-      </Container>
-    </Section>
+    <Container className={styles.container}>
+      <div className={styles.descriptionSection}>
+        <h1 className={styles.title}>Join our growing community</h1>
+        <p className={styles.description}>
+          BotD is an open source project supported by contributing developers across the globe.
+        </p>
+        <Button href='/case-studies/edtech/' variant='outline' size='big' className={styles.button}>
+          Discord
+        </Button>
+      </div>
+    </Container>
   )
 }
