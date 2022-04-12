d60ca2f7f4234825912b3d303f9a020ad0cf1abf
Natanael Pablo Galfrascoli <47128448+natapg@users.noreply.github.com>
2022-04-12 09:49:37 -0300

    fix: moved preconnect tags to gatsby ssr (#584)

---
+++ gatsby-ssr.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {
+  FPJS_SCRIPT_URL_PATTERN,
+  FPJS_VISITORS_ENDPOINT,
+  FPJS_TLS_ENDPOINT,
+  FPJS_INGRESS_ENDPOINT,
+} from './src/constants/env'
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <link rel='preconnect' key='dns-prefetch-google-analytics' href='https://www.google-analytics.com' />,
+    <link rel='preconnect' key='google-tag-manager' href='https://www.googletagmanager.com' />,
+    <link rel='preconnect' key='cdn-segment' href='https://cdn.segment.com' />,
+    <link rel='preconnect' key='api-mapbox' href='https://api.mapbox.com' />,
+
+    FPJS_SCRIPT_URL_PATTERN && (
+      <link rel='preconnect' key='fpjs-script' href={`https://${FPJS_SCRIPT_URL_PATTERN.split('/')[2]}`} />
+    ),
+    FPJS_VISITORS_ENDPOINT && (
+      <link
+        crossOrigin='anonymous'
+        rel='preconnect'
+        key='fpjs-visitors-crossOrigin'
+        href={`https://${FPJS_VISITORS_ENDPOINT.split('/')[2]}`}
+        as='fetch'
+      />
+    ),
+    FPJS_TLS_ENDPOINT && <link rel='preconnect' key='fpjs-tls' href={FPJS_TLS_ENDPOINT} crossOrigin='anonymous' />,
+    FPJS_INGRESS_ENDPOINT && <link rel='preconnect' key='fpjs-ingress' href={FPJS_INGRESS_ENDPOINT} />,
+  ])
+}
