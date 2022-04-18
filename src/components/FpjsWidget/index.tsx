7ecb74899694d8ab63ca3da2c99c5ec0f1930a52
Natanael Pablo Galfrascoli <47128448+natapg@users.noreply.github.com>
2022-04-18 17:39:10 -0300

    Fix: added visits loading state (#587)

--- src/components/FpjsWidget/index.tsx
+++ src/components/FpjsWidget/index.tsx
@@ -5,7 +5,6 @@ import { ReactComponent as IncognitoSvg } from './incognito.svg'
 import Tippy from '@tippyjs/react'
 import classNames from 'classnames'
 import { VisitorResponse } from '../../types/visitorResponse'
-import { CurrentVisitProps } from '../../types/currentVisitProps'
 import MobileWidget, { MobileLoadingState } from './MobileWidget'
 import { useVisitorData } from '@fingerprintjs/fingerprintjs-pro-react'
 import useRollbar from '../../hooks/useRollbar'
@@ -26,26 +25,48 @@ export default memo(function FpjsWidget() {
   const [visits, setVisits] = useState<VisitorResponse[]>([])
   const [isLoading, setIsLoading] = useState(true)
   const [isLoaded, setIsLoaded] = useState(false)
-  const [hasError, setHasError] = useState(false)
-  const { getData, error } = useVisitorData(getConfig, { immediate: false })
+
   const rollbar = useRollbar()
+
+  const {
+    getData,
+    error: identificationError,
+    isLoading: fpjsIsLoading,
+  } = useVisitorData(getConfig, { immediate: false })
+  const [historyLoadError, setHistoryLoadError] = useState(false)
+  const hasError = identificationError != null || historyLoadError
 
   useEffect(() => {
     let isCancelled = false
     setIsLoading(true)
-    if (error) {
-      setHasError(true)
-      return
-    }
 
     async function fetchVisits() {
       if (!visitorId) {
         const data = await getData(true)
+
         setVisitorId(data?.visitorId)
+        const visitsFpjs = [
+          {
+            ...data,
+            timestamp: 1,
+            browserDetails: {
+              botProbability: data?.bot?.probability,
+              browserName: data?.browserName,
+              os: data?.os,
+              osVersion: data?.osVersion,
+              device: data?.device,
+            },
+          },
+        ] as VisitorResponse[]
+
+        setVisits(visitsFpjs)
+        setCurrentVisit(visitsFpjs[0])
       }
+
       if (!visitorId) {
         return
       }
+
       try {
         const { visits } = await loadFpjsHistory(endpoint, visitorId, secretToken)
 
@@ -56,7 +77,7 @@ export default memo(function FpjsWidget() {
         }
       } catch (e) {
         if (e) {
-          setHasError(true)
+          setHistoryLoadError(true)
           rollbar.error('Unable to load visits', getErrorMessage(e))
         }
       } finally {
@@ -70,72 +91,208 @@ export default memo(function FpjsWidget() {
     return () => {
       isCancelled = true
     }
-  }, [getData, visitorId, rollbar, error])
+  }, [getData, visitorId, rollbar])
+
+  if (hasError) {
+    return <LoadingState hasError />
+  }
+
+  if (fpjsIsLoading && isLoading) {
+    return <LoadingState isVisitHistoryLoading={isLoading} isVisitDetailsLoading={fpjsIsLoading} />
+  }
 
   return (
-    <div className={styles.container}>
-      {isLoading || hasError ? (
-        <>
-          <LoadingState hasError={hasError} />
-          <MobileLoadingState />
-        </>
-      ) : (
-        <div
-          className={classNames(styles.demo, styles.desktopOnly, {
-            [styles.loaded]: isLoaded,
-            [styles.incognito]: currentVisit?.incognito,
-          })}
-        >
-          <div className={styles.history}>
-            <div className={styles.header}>
-              Visit History
-              <Tippy content='FingerprintJS Pro allows you to get a history of visits with all available information'>
-                <InfoSvg tabIndex={0} />
-              </Tippy>
-            </div>
-            <div className={styles.content}>
-              <ul className={styles.visits}>
-                {visits &&
-                  visits.map(({ requestId, timestamp, incognito }, i) => {
-                    return (
-                      <li
-                        className={classNames(
-                          styles.visit,
-                          { [styles.selected]: currentVisit?.requestId === requestId },
-                          { [styles.incognito]: incognito },
-                          { [styles.now]: i === 0 }
-                        )}
-                        id={`visit_${requestId}`}
-                        key={requestId}
-                        onClick={() => setCurrentVisit(visits[i])}
-                      >
-                        <span className={styles.visitLabel}>
-                          {i === 0 ? 'Current visit' : getVisitTitle(timestamp)}
-                        </span>
-                        {incognito && <IncognitoSvg />}
-                      </li>
-                    )
-                  })}
-              </ul>
-            </div>
-          </div>
-          {visitorId && <CurrentVisit currentVisit={currentVisit} visits={visits} visitorId={visitorId} />}
-        </div>
-      )}
+    <FpjsWidgetLoaded
+      isVisitHistoryLoading={isLoading}
+      isLoaded={isLoaded}
+      currentVisit={currentVisit}
+      visits={visits}
+      visitorId={visitorId}
+      setCurrentVisit={setCurrentVisit}
+    />
+  )
+})
 
-      {!isLoading && visitorId && (
+async function loadFpjsHistory(endpoint: string, visitorId: string, secretToken: string) {
+  const response = await fetch(`${endpoint}${visitorId}?token=${secretToken}&limit=20`)
+  return await response.json()
+}
+
+interface LoadingStateProps {
+  isVisitDetailsLoading?: boolean
+  isVisitHistoryLoading?: boolean
+  hasError?: boolean
+}
+
+function LoadingState({ isVisitDetailsLoading, isVisitHistoryLoading, hasError }: LoadingStateProps) {
+  return (
+    <div className={styles.container}>
+      <div className={classNames(styles.demo, styles.desktopOnly)}>
+        <VisitHistory isLoading={isVisitHistoryLoading ?? hasError} />
+        <VisitDetails isLoading={isVisitDetailsLoading ?? hasError} />
+
+        {hasError && (
+          <div className={styles.errorMessage}>
+            <h2 className={styles.tryMessage}>An error occurred.</h2>
+            <h2 className={styles.tryMessage}>Please refresh the page or try in incognito mode.</h2>
+          </div>
+        )}
+      </div>
+      <MobileLoadingState />
+    </div>
+  )
+}
+
+interface FpjsWidgetLoadedProps extends VisitDetailsProps {
+  isLoaded: boolean
+  isVisitHistoryLoading: boolean
+}
+
+function FpjsWidgetLoaded({
+  isLoaded,
+  currentVisit,
+  visits,
+  visitorId,
+  setCurrentVisit,
+  isVisitHistoryLoading,
+}: FpjsWidgetLoadedProps) {
+  return (
+    <div className={styles.container}>
+      <div
+        className={classNames(styles.demo, styles.desktopOnly, {
+          [styles.loaded]: isLoaded,
+          [styles.incognito]: currentVisit?.incognito,
+        })}
+      >
+        <VisitHistory
+          isLoading={isVisitHistoryLoading}
+          visits={visits}
+          currentVisit={currentVisit}
+          setCurrentVisit={setCurrentVisit}
+        />
+        {visitorId ? ( // show noJS error
+          <VisitDetails visits={visits} currentVisit={currentVisit} visitorId={visitorId} />
+        ) : (
+          <>
+            <VisitDetails isLoading={true} />
+            <noscript className={styles.errorMessage}>
+              <h1 className={styles.message}>Enable JS to run the demo</h1>
+            </noscript>
+          </>
+        )}
+      </div>
+      {visits && visitorId && (
         <MobileWidget isLoaded={isLoaded} visitorId={visitorId} visits={visits} currentVisit={currentVisit} />
       )}
     </div>
   )
-})
+}
 
-function CurrentVisit({ currentVisit, visits, visitorId }: CurrentVisitProps) {
+interface VisitHistoryProps {
+  isLoading?: boolean
+  visits?: VisitorResponse[]
+  currentVisit?: VisitorResponse
+  setCurrentVisit?: React.Dispatch<React.SetStateAction<VisitorResponse>>
+}
+
+function VisitHistory({ isLoading, visits, currentVisit, setCurrentVisit }: VisitHistoryProps) {
+  if (isLoading) {
+    return (
+      <>
+        <div className={styles.history}>
+          <div className={styles.header}>
+            <Skeleton width={136} height={22} className={styles.headerLoading} />
+          </div>
+          <div className={styles.content}>
+            <ul className={styles.visits}>
+              {repeatElement(8, (i) => (
+                <Skeleton key={i} width={160} height={30} className={styles.visitLoading} />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </>
+    )
+  }
+
+  return (
+    <div className={styles.history}>
+      <div className={styles.header}>
+        Visit History
+        <Tippy content='FingerprintJS Pro allows you to get a history of visits with all available information'>
+          <InfoSvg tabIndex={0} />
+        </Tippy>
+      </div>
+      <div className={styles.content}>
+        <ul className={styles.visits}>
+          {visits &&
+            setCurrentVisit &&
+            visits.map(({ requestId, timestamp, incognito }, i) => {
+              return (
+                <li
+                  className={classNames(
+                    styles.visit,
+                    { [styles.selected]: currentVisit?.requestId === requestId },
+                    { [styles.incognito]: incognito },
+                    { [styles.now]: i === 0 }
+                  )}
+                  id={`visit_${requestId}`}
+                  key={requestId}
+                  onClick={() => setCurrentVisit(visits[i])}
+                >
+                  <span className={styles.visitLabel}>{i === 0 ? 'Current visit' : getVisitTitle(timestamp)}</span>
+                  {incognito && <IncognitoSvg />}
+                </li>
+              )
+            })}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+interface VisitDetailsProps extends VisitHistoryProps {
+  visitorId?: string
+}
+
+function VisitDetails({ isLoading, visits, visitorId, currentVisit }: VisitDetailsProps) {
+  if (isLoading) {
+    return (
+      <div className={styles.currentVisit}>
+        <div className={styles.header}>
+          <Skeleton width={156} height={22} className={styles.headerLoading} />
+        </div>
+        <div className={styles.content}>
+          <div className={styles.visitId}>
+            <Skeleton width={396} height={32} />
+          </div>
+          <div className={classNames(styles.info, styles.bot)}>
+            <Skeleton width={160} height={22} />
+          </div>
+          <div className={classNames(styles.info, styles.ip)}>
+            <Skeleton width={160} height={22} />
+          </div>
+          <div className={classNames(styles.info, styles.incognito)}>
+            <Skeleton width={160} height={22} />
+          </div>
+          <div className={classNames(styles.info, styles.browser)}>
+            <Skeleton width={160} height={22} className={styles.loadingBrowser} />
+          </div>
+          <div className={classNames(styles.info, styles.location)}>
+            <Skeleton width={188} height={22} />
+            <Skeleton square />
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className={styles.currentVisit}>
       <div className={styles.header}>
         <div className={styles.title}>
           {currentVisit &&
+            visits &&
             (currentVisit.requestId === visits[0].requestId
               ? 'Your Current Visit'
               : getVisitTitle(currentVisit.timestamp))}
@@ -197,75 +354,6 @@ function CurrentVisit({ currentVisit, visits, visitorId }: CurrentVisitProps) {
               />
             )}
           </div>
-        </div>
-      </div>
-    </div>
-  )
-}
-
-async function loadFpjsHistory(endpoint: string, visitorId: string, secretToken: string) {
-  const response = await fetch(`${endpoint}${visitorId}?token=${secretToken}&limit=20`)
-  return await response.json()
-}
-
-interface LoadingStateProps {
-  hasError: boolean
-}
-
-function LoadingState({ hasError }: LoadingStateProps) {
-  return (
-    <div className={classNames(styles.demo, styles.desktopOnly)}>
-      <div className={styles.history}>
-        <div className={styles.header}>
-          <Skeleton width={136} height={22} className={styles.headerLoading} />
-        </div>
-        <div className={styles.content}>
-          <ul className={styles.visits}>
-            {repeatElement(8, (i) => (
-              <Skeleton key={i} width={160} height={30} className={styles.visitLoading} />
-            ))}
-          </ul>
-        </div>
-      </div>
-      <CurrentVisitLoading />
-      <noscript className={styles.errorMessage}>
-        <h1 className={styles.message}>Enable JS to run the demo</h1>
-      </noscript>
-      {hasError && (
-        <div className={styles.errorMessage}>
-          <h2 className={styles.tryMessage}>An error occurred.</h2>
-          <h2 className={styles.tryMessage}>Please refresh the page or try in incognito mode.</h2>
-        </div>
-      )}
-    </div>
-  )
-}
-
-function CurrentVisitLoading() {
-  return (
-    <div className={styles.currentVisit}>
-      <div className={styles.header}>
-        <Skeleton width={156} height={22} className={styles.headerLoading} />
-      </div>
-      <div className={styles.content}>
-        <div className={styles.visitId}>
-          <Skeleton width={396} height={32} />
-        </div>
-        <div className={classNames(styles.info, styles.bot)}>
-          <Skeleton width={160} height={22} />
-        </div>
-        <div className={classNames(styles.info, styles.ip)}>
-          <Skeleton width={160} height={22} />
-        </div>
-        <div className={classNames(styles.info, styles.incognito)}>
-          <Skeleton width={160} height={22} />
-        </div>
-        <div className={classNames(styles.info, styles.browser)}>
-          <Skeleton width={160} height={22} className={styles.loadingBrowser} />
-        </div>
-        <div className={classNames(styles.info, styles.location)}>
-          <Skeleton width={188} height={22} />
-          <Skeleton square />
         </div>
       </div>
     </div>
