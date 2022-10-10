5c34839dddcb128ed373c7210104d83d18390d22
Natanael Pablo Galfrascoli <47128448+natapg@users.noreply.github.com>
2022-10-10 14:50:58 -0300

    fix: moved gtm script to head (#759)

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -73,7 +73,7 @@ export function LayoutTemplate({ children, notificationBar }: LayoutTemplateProp
   return (
     // add rel=preconnect resources to gatsby-ssr.js
     <>
-      <Script>{`dataLayer = ${JSON.stringify(defaultDataLayer)};`}</Script>
+      <Script id='gtm-dataLayer'>{`dataLayer = ${JSON.stringify(defaultDataLayer)};`}</Script>
       <Script id='gtm-init'>
         {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','${GTM_TOKEN}');`}
       </Script>
