7f74618a05116a363102a177ff0abd372f910781
Natanael Pablo Galfrascoli <47128448+natapg@users.noreply.github.com>
2022-12-14 11:41:41 -0300

    fix: show webinars instead of webinar articles (#864)
    * fix: show webinars instead of webinar articles
    
    * added empty line

--- src/templates/blog-tag.tsx
+++ src/templates/blog-tag.tsx
@@ -35,7 +35,11 @@ export default function BlogTag({ data, pageContext }: BlogTagProps) {
 
       <Section>
         <Container size='large'>
-          <h1 className={styles.title}>{`${kebabToTitle(tag)}`} Articles</h1>
+          {tag === 'webinar' ? (
+            <h1 className={styles.title}>Webinars</h1>
+          ) : (
+            <h1 className={styles.title}>{`${kebabToTitle(tag)}`} Articles</h1>
+          )}
 
           <Posts
             posts={posts
@@ -81,7 +85,11 @@ export function Head(props: HeadProps<Queries.BlogTagQuery, BlogTagContext>) {
   return (
     <SEO
       pathname={props.location.pathname}
-      title={`${kebabToTitle(props.pageContext.tag)} Articles - Fingerprint Blog | Fingerprint`}
+      title={
+        props.pageContext.tag === 'webinar'
+          ? `Webinars - Fingerprint Blog | Fingerprint`
+          : `${kebabToTitle(props.pageContext.tag)} Articles - Fingerprint Blog | Fingerprint`
+      }
       description={`We are an open source powered company working to prevent online fraud for websites of all sizes. Read our articles on ${kebabToTitle(
         props.pageContext.tag
       )} on our blog.`}
